31d4bc3bff635850daf76e48409ea0463c72c8d0
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-19 11:44:31 +0100

    Reimplemented Movies component using hooks

--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -1,26 +1,22 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import "./Movies.css";
 import MovieListItem from "./MovieListItem";
 import Button from "../Button";
 
 const apiRoot = "https://api.themoviedb.org/3";
 
-export default class Movies extends Component {
-    state = {
-        movies: [],
-        page: 1,
-        pages: 1,
-        query: ""
-    };
+function assembleQuery(filters, page) {
+    return `&page=${page}` + filters;
+}
 
-    filters;
+const Movies = ({filters}) => {
+    const [page, setPage] = useState(1);
+    const [pages, setPages] = useState(1);
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
 
-    static assembleQuery(filters, page) {
-        return `&page=${page}` + filters;
-    };
-
-    loadMovies = query => {
-        this.setState({movies: []});
+    function loadMovies(q) {
+        setMovies([]);
         const url =
             `${apiRoot}/discover/movie` +
             `?api_key=${process.env.REACT_APP_TMDB_API_KEY}` +
@@ -28,7 +24,7 @@ export default class Movies extends Component {
             `&sort_by=popularity.desc` +
             `&include_adult=false` +
             `&include_video=false` +
-            query;
+            q;
         fetch(url)
             .then(async r => {
                 const data = await r.json();
@@ -36,52 +32,44 @@ export default class Movies extends Component {
                     const {vote_count, id, genre_ids, poster_path, title, vote_average, release_date} = result;
                     return {vote_count, id, genre_ids, poster_path, title, vote_average, release_date};
                 });
-                const pages = data.total_pages;
-                this.setState({movies, pages});
+                setPages(data.total_pages);
+                setMovies(movies);
             })
             .catch(console.error);
-    };
+    }
 
-    setPage = page => {
+    function setClampedPage(page) {
         if (page < 1) {
             page = 1;
-        } else if (page > this.state.pages) {
-            page = this.state.pages;
+        } else if (page > pages) {
+            page = pages;
         }
-        if (page !== this.state.page) {
-            this.setState({page, query: Movies.assembleQuery(this.props.filters, page)});
-        }
-    };
-
-    componentDidMount() {
-        this.setState({query: Movies.assembleQuery(this.props.filters, this.state.page)})
+        setPage(page);
     }
 
-    componentWillUpdate(nextProps, nextState) {
-        if (nextState.query !== this.state.query) {
-            this.loadMovies(nextState.query);
+    useEffect(() => {
+        const newQuery = assembleQuery(filters, page);
+        if (query !== newQuery) {
+            setQuery(newQuery);
+            loadMovies(newQuery);
         }
-        if (nextProps.filters !== this.props.filters) {
-            this.setState({query: Movies.assembleQuery(nextProps.filters, this.state.page)})
-        }
-    }
+    });
 
-    render() {
-        return (
-            <section>
-                <ul className="movies">
-                    {
-                        this.state.movies.map(it =>
-                            <MovieListItem movie={it} key={it.id}/>
-                        )
-                    }
-                </ul>
-                <div className="pagination">
-                    <Button onClick={() => this.setPage(this.state.page - 1)}>Previous</Button>
-                    <span>Page {this.state.page}</span>
-                    <Button onClick={() => this.setPage(this.state.page + 1)}>Next</Button>
-                </div>
-            </section>
-        )
-    }
-}
+    return (
+        <section>
+            <ul className="movies">
+                {
+                    movies.map(it =>
+                        <MovieListItem movie={it} key={it.id}/>
+                    )
+                }
+            </ul>
+            <div className="pagination">
+                <Button onClick={() => setClampedPage(page - 1)}>Previous</Button>
+                <span>Page {page}</span>
+                <Button onClick={() => setClampedPage(page + 1)}>Next</Button>
+            </div>
+        </section>
+    );
+};
+export default Movies;
