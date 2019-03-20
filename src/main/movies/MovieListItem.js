b7f7e79ecf5e6441f4db9c02a2881a9548caa201
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 09:33:49 +0100

    Link to movie details page

--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -1,28 +1,31 @@
 import React from "react";
 import "./MovieListItem.css";
+import {Link} from "react-router-dom";
 
 const posterRoot = "https://image.tmdb.org/t/p/w342";
 
 const MovieListItem = ({movie}) => {
-    const {title, vote_average: rating, poster_path, release_date: release} = movie;
+    const {id, title, vote_average: rating, poster_path, release_date: release} = movie;
     const imgUrl = `${posterRoot}/${poster_path}`;
 
     return (
         <li className="movie-item">
-            <img src={imgUrl} alt=""/>
-            <div className="movie-description">
-                <h2>{title}</h2>
-                <section className="movie-details">
-                    <div className="movie-year">
-                        <span className="title">Year</span>
-                        <span>{release.substr(0, 4)}</span>
-                    </div>
-                    <div className="movie-rating">
-                        <span className="title">Rating</span>
-                        <span>{rating}</span>
-                    </div>
-                </section>
-            </div>
+            <Link to={`/movies/${id}`} className="thumbnail">
+                <img src={imgUrl} alt=""/>
+                <div className="movie-description">
+                    <h2>{title}</h2>
+                    <section className="movie-details">
+                        <div className="movie-year">
+                            <span className="title">Year</span>
+                            <span>{release.substr(0, 4)}</span>
+                        </div>
+                        <div className="movie-rating">
+                            <span className="title">Rating</span>
+                            <span>{rating}</span>
+                        </div>
+                    </section>
+                </div>
+            </Link>
         </li>
     );
 };
