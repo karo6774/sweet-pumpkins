e9ff30158f3625d7e504783e2794c9635bd1ae50
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 10:31:46 +0100

    Reimplemented App as function component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Header from "./header/Header";
 import Movie from "./main/movie/Movie";
 import Main from "./main/Main";
 import NotFound from "./NotFound";
 
-export default class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Header/>
-                    <Switch>
-                        <Route exact path="/" component={Main}/>
-                        <Route path="/movies/:id" component={Movie}/>
-                        <Route component={NotFound}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
-}
+const App = () => (
+    <BrowserRouter>
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/" component={Main}/>
+                <Route path="/movies/:id" component={Movie}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+export default App;
