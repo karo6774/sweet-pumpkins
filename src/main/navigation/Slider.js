49341087c1f7e8c4fb162beab9b5020f67eb9dd6
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 10:30:24 +0100

    Reimplemented Slider as function component

--- src/main/navigation/Slider.js
+++ src/main/navigation/Slider.js
@@ -3,24 +3,18 @@ import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
 import "./Slider.css";
 
-export default class Slider extends React.Component {
-    onChange = range => {
-        this.props.onChange(range);
-    };
-
-    render() {
-        const {min, max, step, value} = this.props.data;
-        return (
-            <div className="slider">
-                <label>{this.props.children}</label>
-                <InputRange
-                    minValue={min}
-                    maxValue={max}
-                    step={step}
-                    onChange={this.onChange}
-                    value={value}
-                />
-            </div>
-        )
-    }
-}
+const Slider = ({children, onChange, data: {min, max, step, value}}) => {
+    return (
+        <div className="slider">
+            <label>{children}</label>
+            <InputRange
+                minValue={min}
+                maxValue={max}
+                step={step}
+                onChange={onChange}
+                value={value}
+            />
+        </div>
+    );
+};
+export default Slider;
