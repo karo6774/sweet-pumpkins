7380821507ef73366a0c1bc73802ff1569fc5ce7
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 10:28:17 +0100

    Reimplemented Navigation as function component

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -1,49 +1,39 @@
-import React, {Component} from "react";
+import React from "react";
 import "./Navigation.css";
 import Selection from "./Selection";
 import Slider from "./Slider";
 import Button from "../Button";
 
-export default class Navigation extends Component {
-    onSearch;
-    onFiltersChanged;
-    filters;
-    genres;
+const Navigation = ({onSearch, onFiltersChanged, filters, genres}) => {
+    const setYear = year => setSliderValue("year", year);
+    const setRating = rating => setSliderValue("rating", rating);
+    const setRuntime = runtime => setSliderValue("runtime", runtime);
 
-    setGenre = genre => {
-        this.props.onFiltersChanged({...this.props.filters, genre});
-    };
-
-    setYear = year => this.setSliderValue("year", year);
-    setRating = rating => this.setSliderValue("rating", rating);
-    setRuntime = runtime => this.setSliderValue("runtime", runtime);
-
-    setSliderValue = (name, value) => {
-        this.props.onFiltersChanged({
-            ...this.props.filters,
+    function setSliderValue(name, value) {
+        onFiltersChanged({
+            ...filters,
             [name]: {
-                ...this.props.filters[name],
+                ...filters[name],
                 value
             }
         });
-    };
-
-    confirmSearch = () => {
-        this.props.onSearch();
-    };
-
-    render() {
-        return (
-            <section className="navigation">
-                <Selection selected={this.props.filters.genre}
-                           values={this.props.genres.map(it => ({display: it.name, value: it.id}))}
-                           onChanged={this.setGenre}>Genre</Selection>
-                <Slider data={this.props.filters.year} onChange={this.setYear}>Year</Slider>
-                <Slider data={this.props.filters.rating} onChange={this.setRating}>Rating</Slider>
-                <Slider data={this.props.filters.runtime} onChange={this.setRuntime}>Runtime</Slider>
-
-                <Button onClick={this.confirmSearch}>Search</Button>
-            </section>
-        );
     }
-}
+
+    function setGenre(genre) {
+        onFiltersChanged({...filters, genre});
+    }
+
+    return (
+        <section className="navigation">
+            <Selection selected={filters.genre}
+                       values={genres.map(it => ({display: it.name, value: it.id}))}
+                       onChanged={setGenre}>Genre</Selection>
+            <Slider data={filters.year} onChange={setYear}>Year</Slider>
+            <Slider data={filters.rating} onChange={setRating}>Rating</Slider>
+            <Slider data={filters.runtime} onChange={setRuntime}>Runtime</Slider>
+
+            <Button onClick={() => onSearch()}>Search</Button>
+        </section>
+    );
+};
+export default Navigation;
