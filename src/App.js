c5923a69285fcd850be8a8fb22037ef1e5fd9fef
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 09:11:25 +0100

    Add Movie details page routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Header from "./header/Header";
+import Movie from "./main/movie/Movie";
 import Main from "./main/Main";
+import NotFound from "./NotFound";
 
 export default class App extends Component {
-  render() {
-    return (
-        <div>
-          <Header/>
-          <Main/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={Main}/>
+                        <Route path="/movie/:id" component={Movie}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
---
+++ src/main/movie/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Movie.css";
+
+const Movie = () => (
+    <div>Movie</div>
+);
+export default Movie;
