215919c15f46aece86a08ac10abbe92e91783b1a
Karoline Halberg Rasmussen <karo6774@edu.campusvejle.dk>
2019-03-20 10:22:15 +0100

    Remove unused import

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import React, {Component, useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import "./Main.css";
 import Navigation from "./navigation/Navigation";
 import Movies from "./movies/Movies";
